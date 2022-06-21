cc417a93cb56b516f771f8889369c52436fe714e
thebesttv <taojuntian@outlook.com>
2022-06-21 22:02:06 +0800

    Use Qt screen capture API even in Linux
    The original code only works in GNOME as it uses the GNOME D-Bus API to
    capture screenshot in Linux.

--- Capture/screengrabber.cpp
+++ Capture/screengrabber.cpp
@@ -1,40 +1,40 @@
 #include "screengrabber.h"
 #include <QPixmap>
-#if defined(Q_OS_LINUX) || defined(Q_OS_UNIX)
-#include <QDir>
-#include <QRandomGenerator>
-#include <QtDBus/QDBusInterface>
-#include <QtDBus/QDBusReply>
-#else
+// #if defined(Q_OS_LINUX) || defined(Q_OS_UNIX)
+// #include <QDir>
+// #include <QRandomGenerator>
+// #include <QtDBus/QDBusInterface>
+// #include <QtDBus/QDBusReply>
+// #else
 #include <QApplication>
 #include <QScreen>
 #include <QDesktopWidget>
-#endif
+// #endif
 #include <QDebug>
 
 QPixmap ScreenGrabber::grabEntireDesktop()
 {
-#if defined(Q_OS_LINUX) || defined(Q_OS_UNIX)
-    //https://gitlab.gnome.org/GNOME/gnome-shell/blob/master/data/dbus-interfaces/org.gnome.Shell.Screenshot.xml
-    QDBusInterface gnomeInterface("org.gnome.Shell", "/org/gnome/Shell/Screenshot",
-                                  "org.gnome.Shell.Screenshot");
-    QString path = QDir::temp().absoluteFilePath(QString("FunOCR_%1.png")
-                                                 .arg(QRandomGenerator::global()->generate()));
-    QDBusReply<bool> reply = gnomeInterface.call("Screenshot", false, false, path);
+// #if defined(Q_OS_LINUX) || defined(Q_OS_UNIX)
+//     //https://gitlab.gnome.org/GNOME/gnome-shell/blob/master/data/dbus-interfaces/org.gnome.Shell.Screenshot.xml
+//     QDBusInterface gnomeInterface("org.gnome.Shell", "/org/gnome/Shell/Screenshot",
+//                                   "org.gnome.Shell.Screenshot");
+//     QString path = QDir::temp().absoluteFilePath(QString("FunOCR_%1.png")
+//                                                  .arg(QRandomGenerator::global()->generate()));
+//     QDBusReply<bool> reply = gnomeInterface.call("Screenshot", false, false, path);
 
-    QPixmap img;
-    if(!reply){
-        qDebug().noquote() << "Screenshot failed";
-    }else{
-        img.load(path);
-        qDebug().noquote() << QString("get entire desktop of size (%1, %2)")
-                              .arg(img.size().width())
-                              .arg(img.size().height());
-        QFile imgFile(path);
-        imgFile.remove();
-    }
-    return img;
-#else
+//     QPixmap img;
+//     if(!reply){
+//         qDebug().noquote() << "Screenshot failed";
+//     }else{
+//         img.load(path);
+//         qDebug().noquote() << QString("get entire desktop of size (%1, %2)")
+//                               .arg(img.size().width())
+//                               .arg(img.size().height());
+//         QFile imgFile(path);
+//         imgFile.remove();
+//     }
+//     return img;
+// #else
     QRect geometry;
     for (QScreen *const screen : QGuiApplication::screens()) {
         geometry = geometry.united(screen->geometry());
@@ -54,5 +54,5 @@ QPixmap ScreenGrabber::grabEntireDesktop()
                           .arg(img.size().width())
                           .arg(img.size().height());
     return img;
-#endif
+// #endif
 }
